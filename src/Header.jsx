3bcff60fad11288792401b8572348418e71625ca
bshin27 <125755440+bshin27@users.noreply.github.com>
2023-11-30 13:25:08 -0600

    init

---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+export function Header() {
+  return (
+    <header>
+      <nav>
+        <a href="#home">Home</a> | <a href="#">Products</a>
+      </nav>
+    </header>
+  );
+}
---
+++ src/ProductsIndex.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable react/prop-types */
+export function ProductsIndex(props) {
+  return (
+    <div>
+      <h1>All Products</h1>
+      {props.products.map((product) => (
+        <div key={product.id}>
+          <h2>{product.name}</h2>
+          <img src="" />
+          <p>Price: {product.price}</p>
+          <p>Description: {product.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
