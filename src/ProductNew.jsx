34e39544235e5cfe3b6c558a1afa1aa184e08891
bshin27 <125755440+bshin27@users.noreply.github.com>
2023-12-01 14:45:04 -0600

    fixed the spacing and image issue

--- src/ProductNew.jsx
+++ src/ProductNew.jsx
@@ -8,24 +8,26 @@ export function ProductNew(props) {
 
   return (
     <div>
-      <h1>New Product</h1>
+      <h1 className="px-4 py-4 t">New Product</h1>
       <form onSubmit={handleSubmit}>
-        <div>
-          Name: <input type="text" name="name" />
+        <div className="px-5">
+          <div>
+            Name: <input type="text" name="name" />
+          </div>
+          <div>
+            Price: <input type="number" name="price" />
+          </div>
+          <div>
+            Description: <input type="text" name="description" />
+          </div>
+          <div>
+            Supplier ID: <input type="number" name="supplier_id" />
+          </div>
+          <div>
+            Stock: <input type="number" name="in_stock" />
+          </div>
+          <button type="submit">Create Product</button>
         </div>
-        <div>
-          Price: <input type="number" name="price" />
-        </div>
-        <div>
-          Description: <input type="text" name="description" />
-        </div>
-        <div>
-          Supplier ID: <input type="number" name="supplier_id" />
-        </div>
-        <div>
-          Stock: <input type="number" name="in_stock" />
-        </div>
-        <button type="submit">Create Product</button>
       </form>
     </div>
   );
--- src/ProductsIndex.jsx
+++ src/ProductsIndex.jsx
@@ -4,26 +4,27 @@ import { Link } from "react-router-dom";
 export function ProductsIndex(props) {
   return (
     <div className="text-center">
-      <h1 className="text-3xl font-bold underline my-6">All Products</h1>
-      <div className="grid grid-cols-3 space-x-3 mx-3 font-serif bg-[ffd14a]">
+      <h1 className="text-3xl font-bold underline my-6 space-x-3">All Products</h1>
+      <div className="md:z-20 grid grid-cols-3 gap-3 mx-3">
         {props.products.map((product) => (
-          <div key={product.id}>
-            <div className="space-y-2 py-4 cotent-center">
+          <div className="font-serif bg-[#ffd14a] rounded-md" key={product.id}>
+            <div className="space-y-2 py-4">
               {product.images.map((image) => (
                 <div key={image.id}>
-                  <img className="max-w-[90%] border-2 border-black rounded-md" src={image.url} alt="card image" />
+                  <div className="flex flex-col items-center">
+                    <img className="max-w-[90%] border-2 border-black rounded-md " src={image.url} alt="card image" />
+                  </div>
                 </div>
               ))}
             </div>
             <h2 className="text-lg font-bold">{product.name}</h2>
             <p className="font-mono">Price: {product.price}</p>
             <p>Description: {product.description}</p>
-            <Link
-              className="max-w-[50%] text-[#f2ecbe] border-2 border-[#f2ecbe] rounded p-0.5 bg-[#c08261] hover:bg-[#f2ecebe] active:bg-violet-700"
-              to="/products/:id"
-            >
-              Product Info
-            </Link>
+            <div className="flex flex-col items-center my-4">
+              <div className="max-w-[50%] text-[#f2ecbe] border-2 border-[#f2ecbe] rounded p-0.5 bg-[#c08261] hover:bg-[#f2ecbe] hover:text-[#c08261] hover:duration-200">
+                <Link to="/products/:id">Product Info</Link>
+              </div>
+            </div>
           </div>
         ))}
       </div>
