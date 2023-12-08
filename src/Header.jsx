5fb49119d6d7c6d53d44ee1308d2f9710d530a51
bshin27 <125755440+bshin27@users.noreply.github.com>
2023-12-08 13:21:14 -0600

    navbar changes when logged in

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,7 +5,6 @@ export function Header() {
   return (
     <header className="bg-[#e2c799] rounded-b-lg p-4">
       <nav>
-        <Link to="/">Home</Link> | <Link to="/products">Products</Link> | <Link to="/products/new">New Product</Link> |{" "}
         {localStorage.jwt === undefined ? (
           <>
             <Link to="/">Home</Link> | <Link to="/products">Products</Link> |{" "}
